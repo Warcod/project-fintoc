c65cd83454a6117d447d156d2d6870d8755b44c9
guido irigoyen <guidoirigoyen@gmail.com>
2023-10-16 13:35:32 -0300

    soporta más de un resultado

--- venv/find_payment_user.py
+++ venv/find_payment_user.py
@@ -1,7 +1,7 @@
 import csv
 
 def get_payment_info_from_csv(amount, csv_file):
-    payment_info = None
+    payment_info_list = []
     with open(csv_file, 'r', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -10,17 +10,17 @@ def get_payment_info_from_csv(amount, csv_file):
                     "amount": row['amount'],
                     "user": row['user']
                 }
-                break
-    return payment_info
+                payment_info_list.append(payment_info)
+    return payment_info_list
 
 # Ejemplo de uso
 amount = 1200000  # Ingresa el monto que estás buscando
 csv_file = 'csv-inic-pagos.csv'  # Reemplaza esto con la ruta real de tu archivo CSV
 
-payment_info = get_payment_info_from_csv(amount, csv_file)
+payment_info_list = get_payment_info_from_csv(amount, csv_file)
 
-if payment_info:
+if payment_info_list:
     print("Información de pago encontrada:")
-    print(payment_info)
+    print(payment_info_list)
 else:
     print("No se encontró información para el monto especificado en el archivo CSV.")
